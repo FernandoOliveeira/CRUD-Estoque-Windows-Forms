b6e41c84d2c5da044096499e0d08aa11d66d2795
Fernando <fernando.silva789@etec.sp.gov.br>
2019-11-04 18:27:19 -0300

    TextBox's e DataGridView adicionados à tela Consultar Produtos

--- Sistema Estoque/ViewLayer/UCConsultar.Designer.cs
+++ Sistema Estoque/ViewLayer/UCConsultar.Designer.cs
@@ -29,22 +29,17 @@
         private void InitializeComponent()
         {
             this.label2 = new System.Windows.Forms.Label();
-            this.tblConsultar = new System.Windows.Forms.TableLayoutPanel();
-            this.dataGridPanel = new System.Windows.Forms.Panel();
             this.dgvConsulta = new System.Windows.Forms.DataGridView();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.consultarNomePanelLine = new System.Windows.Forms.Panel();
             this.txtConsultarNome = new System.Windows.Forms.TextBox();
             this.rdbConsultarNome = new System.Windows.Forms.RadioButton();
-            this.panel3 = new System.Windows.Forms.Panel();
             this.txtConsultarCodigo = new System.Windows.Forms.TextBox();
             this.rdbConsultarCodigo = new System.Windows.Forms.RadioButton();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.tblConsultar.SuspendLayout();
-            this.dataGridPanel.SuspendLayout();
+            this.consultarCodigoPanelLine = new System.Windows.Forms.Panel();
+            this.txtCodBarras = new System.Windows.Forms.TextBox();
+            this.rdbConsultarCodBarras = new System.Windows.Forms.RadioButton();
+            this.consultarCodBarrasPanelLine = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dgvConsulta)).BeginInit();
-            this.panel1.SuspendLayout();
-            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // label2
@@ -59,37 +54,6 @@
             this.label2.Text = "Consultar Produtos";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // tblConsultar
-            // 
-            this.tblConsultar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tblConsultar.ColumnCount = 2;
-            this.tblConsultar.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tblConsultar.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tblConsultar.Controls.Add(this.dataGridPanel, 0, 2);
-            this.tblConsultar.Controls.Add(this.panel1, 0, 0);
-            this.tblConsultar.Controls.Add(this.panel3, 1, 0);
-            this.tblConsultar.Location = new System.Drawing.Point(7, 42);
-            this.tblConsultar.Name = "tblConsultar";
-            this.tblConsultar.RowCount = 3;
-            this.tblConsultar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 58.13953F));
-            this.tblConsultar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 41.86047F));
-            this.tblConsultar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 264F));
-            this.tblConsultar.Size = new System.Drawing.Size(720, 401);
-            this.tblConsultar.TabIndex = 3;
-            // 
-            // dataGridPanel
-            // 
-            this.dataGridPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tblConsultar.SetColumnSpan(this.dataGridPanel, 2);
-            this.dataGridPanel.Controls.Add(this.dgvConsulta);
-            this.dataGridPanel.Location = new System.Drawing.Point(3, 139);
-            this.dataGridPanel.Name = "dataGridPanel";
-            this.dataGridPanel.Size = new System.Drawing.Size(714, 259);
-            this.dataGridPanel.TabIndex = 0;
-            // 
             // dgvConsulta
             // 
             this.dgvConsulta.AllowUserToAddRows = false;
@@ -102,35 +66,24 @@
             this.dgvConsulta.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(112)))), ((int)(((byte)(147)))));
             this.dgvConsulta.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dgvConsulta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvConsulta.Location = new System.Drawing.Point(3, 0);
+            this.dgvConsulta.Location = new System.Drawing.Point(12, 272);
             this.dgvConsulta.MultiSelect = false;
             this.dgvConsulta.Name = "dgvConsulta";
             this.dgvConsulta.ReadOnly = true;
             this.dgvConsulta.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvConsulta.Size = new System.Drawing.Size(708, 232);
+            this.dgvConsulta.Size = new System.Drawing.Size(634, 139);
             this.dgvConsulta.TabIndex = 0;
             // 
-            // panel1
+            // consultarNomePanelLine
             // 
-            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.consultarNomePanelLine.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.Controls.Add(this.panel2);
-            this.panel1.Controls.Add(this.txtConsultarNome);
-            this.panel1.Controls.Add(this.rdbConsultarNome);
-            this.panel1.Location = new System.Drawing.Point(3, 3);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(354, 73);
-            this.panel1.TabIndex = 1;
-            // 
-            // panel2
-            // 
-            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel2.BackColor = System.Drawing.Color.Black;
-            this.panel2.Location = new System.Drawing.Point(5, 40);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 1);
-            this.panel2.TabIndex = 2;
+            this.consultarNomePanelLine.BackColor = System.Drawing.Color.Black;
+            this.consultarNomePanelLine.ForeColor = System.Drawing.Color.Black;
+            this.consultarNomePanelLine.Location = new System.Drawing.Point(12, 114);
+            this.consultarNomePanelLine.Name = "consultarNomePanelLine";
+            this.consultarNomePanelLine.Size = new System.Drawing.Size(191, 1);
+            this.consultarNomePanelLine.TabIndex = 2;
             // 
             // txtConsultarNome
             // 
@@ -142,35 +95,25 @@
             this.txtConsultarNome.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtConsultarNome.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtConsultarNome.ForeColor = System.Drawing.Color.White;
-            this.txtConsultarNome.Location = new System.Drawing.Point(4, 24);
+            this.txtConsultarNome.Location = new System.Drawing.Point(12, 97);
             this.txtConsultarNome.Name = "txtConsultarNome";
-            this.txtConsultarNome.Size = new System.Drawing.Size(200, 19);
+            this.txtConsultarNome.Size = new System.Drawing.Size(191, 19);
             this.txtConsultarNome.TabIndex = 1;
             // 
             // rdbConsultarNome
             // 
             this.rdbConsultarNome.AutoSize = true;
+            this.rdbConsultarNome.Checked = true;
             this.rdbConsultarNome.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rdbConsultarNome.ForeColor = System.Drawing.Color.White;
-            this.rdbConsultarNome.Location = new System.Drawing.Point(3, 3);
+            this.rdbConsultarNome.Location = new System.Drawing.Point(11, 76);
             this.rdbConsultarNome.Name = "rdbConsultarNome";
             this.rdbConsultarNome.Size = new System.Drawing.Size(266, 24);
             this.rdbConsultarNome.TabIndex = 0;
             this.rdbConsultarNome.TabStop = true;
             this.rdbConsultarNome.Text = "Consultar pelo nome do produto";
             this.rdbConsultarNome.UseVisualStyleBackColor = true;
-            // 
-            // panel3
-            // 
-            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel3.Controls.Add(this.txtConsultarCodigo);
-            this.panel3.Controls.Add(this.rdbConsultarCodigo);
-            this.panel3.Controls.Add(this.panel4);
-            this.panel3.Location = new System.Drawing.Point(363, 3);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(354, 73);
-            this.panel3.TabIndex = 2;
+            this.rdbConsultarNome.CheckedChanged += new System.EventHandler(this.rdbConsultarNome_CheckedChanged);
             // 
             // txtConsultarCodigo
             // 
@@ -180,11 +123,12 @@
             this.txtConsultarCodigo.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.RecentlyUsedList;
             this.txtConsultarCodigo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(79)))), ((int)(((byte)(103)))));
             this.txtConsultarCodigo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtConsultarCodigo.Enabled = false;
             this.txtConsultarCodigo.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtConsultarCodigo.ForeColor = System.Drawing.Color.White;
-            this.txtConsultarCodigo.Location = new System.Drawing.Point(4, 24);
+            this.txtConsultarCodigo.Location = new System.Drawing.Point(12, 166);
             this.txtConsultarCodigo.Name = "txtConsultarCodigo";
-            this.txtConsultarCodigo.Size = new System.Drawing.Size(200, 19);
+            this.txtConsultarCodigo.Size = new System.Drawing.Size(191, 19);
             this.txtConsultarCodigo.TabIndex = 1;
             // 
             // rdbConsultarCodigo
@@ -192,57 +136,100 @@
             this.rdbConsultarCodigo.AutoSize = true;
             this.rdbConsultarCodigo.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rdbConsultarCodigo.ForeColor = System.Drawing.Color.White;
-            this.rdbConsultarCodigo.Location = new System.Drawing.Point(3, 3);
+            this.rdbConsultarCodigo.Location = new System.Drawing.Point(11, 145);
             this.rdbConsultarCodigo.Name = "rdbConsultarCodigo";
             this.rdbConsultarCodigo.Size = new System.Drawing.Size(277, 24);
             this.rdbConsultarCodigo.TabIndex = 0;
             this.rdbConsultarCodigo.TabStop = true;
             this.rdbConsultarCodigo.Text = "Consultar pelo código do produto";
             this.rdbConsultarCodigo.UseVisualStyleBackColor = true;
+            this.rdbConsultarCodigo.CheckedChanged += new System.EventHandler(this.rdbConsultarCodigo_CheckedChanged);
             // 
-            // panel4
+            // consultarCodigoPanelLine
             // 
-            this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.consultarCodigoPanelLine.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel4.BackColor = System.Drawing.Color.Black;
-            this.panel4.Location = new System.Drawing.Point(5, 40);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(200, 1);
-            this.panel4.TabIndex = 3;
+            this.consultarCodigoPanelLine.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(125)))), ((int)(((byte)(125)))));
+            this.consultarCodigoPanelLine.Location = new System.Drawing.Point(12, 183);
+            this.consultarCodigoPanelLine.Name = "consultarCodigoPanelLine";
+            this.consultarCodigoPanelLine.Size = new System.Drawing.Size(191, 1);
+            this.consultarCodigoPanelLine.TabIndex = 3;
+            // 
+            // txtCodBarras
+            // 
+            this.txtCodBarras.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCodBarras.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(79)))), ((int)(((byte)(103)))));
+            this.txtCodBarras.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtCodBarras.Enabled = false;
+            this.txtCodBarras.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodBarras.ForeColor = System.Drawing.Color.White;
+            this.txtCodBarras.Location = new System.Drawing.Point(12, 232);
+            this.txtCodBarras.Name = "txtCodBarras";
+            this.txtCodBarras.Size = new System.Drawing.Size(191, 19);
+            this.txtCodBarras.TabIndex = 1;
+            // 
+            // rdbConsultarCodBarras
+            // 
+            this.rdbConsultarCodBarras.AutoSize = true;
+            this.rdbConsultarCodBarras.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbConsultarCodBarras.ForeColor = System.Drawing.Color.White;
+            this.rdbConsultarCodBarras.Location = new System.Drawing.Point(11, 211);
+            this.rdbConsultarCodBarras.Name = "rdbConsultarCodBarras";
+            this.rdbConsultarCodBarras.Size = new System.Drawing.Size(264, 24);
+            this.rdbConsultarCodBarras.TabIndex = 0;
+            this.rdbConsultarCodBarras.TabStop = true;
+            this.rdbConsultarCodBarras.Text = "Consultar pelo código de barras";
+            this.rdbConsultarCodBarras.UseVisualStyleBackColor = true;
+            this.rdbConsultarCodBarras.CheckedChanged += new System.EventHandler(this.rdbConsultarCodBarras_CheckedChanged);
+            // 
+            // consultarCodBarrasPanelLine
+            // 
+            this.consultarCodBarrasPanelLine.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.consultarCodBarrasPanelLine.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(125)))), ((int)(((byte)(125)))));
+            this.consultarCodBarrasPanelLine.Location = new System.Drawing.Point(12, 249);
+            this.consultarCodBarrasPanelLine.Name = "consultarCodBarrasPanelLine";
+            this.consultarCodBarrasPanelLine.Size = new System.Drawing.Size(191, 1);
+            this.consultarCodBarrasPanelLine.TabIndex = 4;
             // 
             // UCConsultar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(79)))), ((int)(((byte)(103)))));
-            this.Controls.Add(this.tblConsultar);
+            this.Controls.Add(this.dgvConsulta);
+            this.Controls.Add(this.consultarNomePanelLine);
+            this.Controls.Add(this.rdbConsultarCodBarras);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtConsultarCodigo);
+            this.Controls.Add(this.rdbConsultarNome);
+            this.Controls.Add(this.consultarCodBarrasPanelLine);
+            this.Controls.Add(this.rdbConsultarCodigo);
+            this.Controls.Add(this.txtConsultarNome);
+            this.Controls.Add(this.txtCodBarras);
+            this.Controls.Add(this.consultarCodigoPanelLine);
             this.Name = "UCConsultar";
             this.Size = new System.Drawing.Size(745, 513);
-            this.tblConsultar.ResumeLayout(false);
-            this.dataGridPanel.ResumeLayout(false);
+            this.Load += new System.EventHandler(this.UCConsultar_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvConsulta)).EndInit();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TableLayoutPanel tblConsultar;
-        private System.Windows.Forms.Panel dataGridPanel;
         private System.Windows.Forms.DataGridView dgvConsulta;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel consultarNomePanelLine;
         private System.Windows.Forms.TextBox txtConsultarNome;
         private System.Windows.Forms.RadioButton rdbConsultarNome;
-        private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.RadioButton rdbConsultarCodigo;
         private System.Windows.Forms.TextBox txtConsultarCodigo;
-        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel consultarCodigoPanelLine;
+        private System.Windows.Forms.TextBox txtCodBarras;
+        private System.Windows.Forms.RadioButton rdbConsultarCodBarras;
+        private System.Windows.Forms.Panel consultarCodBarrasPanelLine;
     }
 }
--- Sistema Estoque/ViewLayer/UCConsultar.cs
+++ Sistema Estoque/ViewLayer/UCConsultar.cs
@@ -18,5 +18,88 @@ namespace Sistema_Estoque.ViewLayer
         }
 
 
+
+
+        private void UCConsultar_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+
+
+
+        private void rdbConsultarNome_CheckedChanged(object sender, EventArgs e)
+        {
+            txtConsultarNome.Focus();
+            txtConsultarNome.Text = "";
+
+            if (rdbConsultarNome.Checked)
+            {
+                txtConsultarNome.Enabled = true;
+                consultarNomePanelLine.BackColor = Color.Black;
+
+
+                txtConsultarCodigo.Enabled = false;
+                txtCodBarras.Enabled = false;
+
+                consultarCodigoPanelLine.BackColor = Color.FromArgb(120, 120, 120);
+                consultarCodBarrasPanelLine.BackColor = Color.FromArgb(120, 120, 120);
+            }
+            else
+            {
+
+            }
+        }
+
+        private void rdbConsultarCodigo_CheckedChanged(object sender, EventArgs e)
+        {
+            txtConsultarCodigo.Focus();
+            txtConsultarCodigo.Text = "";
+
+            if (rdbConsultarCodigo.Checked)
+            {
+                txtConsultarCodigo.Enabled = true;
+                consultarCodigoPanelLine.BackColor = Color.Black;
+
+
+                txtConsultarNome.Enabled = false;
+                txtCodBarras.Enabled = false;
+
+                consultarNomePanelLine.BackColor = Color.FromArgb(120, 120, 120);
+                consultarCodBarrasPanelLine.BackColor = Color.FromArgb(120, 120, 120);
+            }
+            else
+            {
+
+            }
+        }
+
+        private void rdbConsultarCodBarras_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            txtCodBarras.Focus();
+            txtCodBarras.Text = "";
+
+            if (rdbConsultarCodBarras.Checked)
+            {
+                txtCodBarras.Enabled = true;
+                consultarCodBarrasPanelLine.BackColor = Color.Black;
+
+
+
+
+                txtConsultarNome.Enabled = false;
+                txtConsultarCodigo.Enabled = false;
+
+                consultarNomePanelLine.BackColor = Color.FromArgb(120, 120, 120);
+                consultarCodigoPanelLine.BackColor = Color.FromArgb(120, 120, 120);
+            }
+            else
+            {
+
+            }
+        }
+
+        
     }
 }
