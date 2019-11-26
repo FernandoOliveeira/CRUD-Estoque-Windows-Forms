48b02126519b70391075172ba0740718c90e3ff7
Fernando <fernando.silva789@etec.sp.gov.br>
2019-11-26 17:53:54 -0300

    Adicionado texto de boas vindas à tela UCHome

--- Sistema Estoque/ViewLayer/UCHome.Designer.cs
+++ Sistema Estoque/ViewLayer/UCHome.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblBoasVindas = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblTitulo
@@ -43,11 +44,25 @@
             this.lblTitulo.Text = "Tela Inicial";
             this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // lblBoasVindas
+            // 
+            this.lblBoasVindas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblBoasVindas.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBoasVindas.ForeColor = System.Drawing.Color.White;
+            this.lblBoasVindas.Location = new System.Drawing.Point(0, 55);
+            this.lblBoasVindas.Name = "lblBoasVindas";
+            this.lblBoasVindas.Size = new System.Drawing.Size(745, 458);
+            this.lblBoasVindas.TabIndex = 4;
+            this.lblBoasVindas.Text = "Seja Bem vindo ao meu primeiro CRUD\r\n\r\nEscolha uma das operações ao lado para com" +
+    "eçar\r\n\r\n";
+            this.lblBoasVindas.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // UCHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(79)))), ((int)(((byte)(103)))));
+            this.Controls.Add(this.lblBoasVindas);
             this.Controls.Add(this.lblTitulo);
             this.Name = "UCHome";
             this.Size = new System.Drawing.Size(745, 513);
@@ -58,5 +73,6 @@
         #endregion
 
         private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblBoasVindas;
     }
 }
